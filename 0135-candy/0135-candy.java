f229f4da121aa5fd0e889dc024e2866f22dc46c7
Shivam Shukla <kulshivam69@gmail.com>
2026-08-18 16:04:53 +0530

    Time: 3 ms (86.47%), Space: 52.1 MB (5.25%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -5,21 +5,31 @@ class Solution {
         int total = 0;
         int[] candies = new int[n];
         Arrays.fill(candies,1);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        for(int i=0;i<n;i++){
-            pq.add(new int[]{i,ratings[i]});
-        }
-        while(!pq.isEmpty()){
-            int[] child = pq.poll();
-            int id = child[0];
-            int rate = child[1];
-            if(id>0 && rate>ratings[id-1])
-                candies[id]=Math.max(candies[id],candies[id-1]+1);
 
-            if(id<n-1 && rate>ratings[id+1])
-                    candies[id]=Math.max(candies[id],candies[id+1]+1);
-
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])
+                candies[i]=candies[i-1]+1;
         }
+
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1])
+                candies[i]=Math.max(candies[i],candies[i+1]+1);
+        }
+
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        // for(int i=0;i<n;i++){
+        //     pq.add(new int[]{i,ratings[i]});
+        // }
+        // while(!pq.isEmpty()){
+        //     int[] child = pq.poll();
+        //     int id = child[0];
+        //     if(id>0 && rate>ratings[id-1])
+        //         candies[id]=Math.max(candies[id],candies[id-1]+1);
+
+        //     if(id<n-1 && rate>ratings[id+1])
+        //             candies[id]=Math.max(candies[id],candies[id+1]+1);
+
+        // }
         for(int i : candies){
             total+=i;
         }
