96f0d70b939e1dc56a3e983d09a4dc1e056828df
Shivam Shukla <kulshivam69@gmail.com>
2026-09-24 16:43:48 +0530

    Time: 1 ms (99.84%), Space: 44.1 MB (77.94%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -17,27 +17,32 @@ class Solution {
     List<String> res;
     public List<String> binaryTreePaths(TreeNode root) {
         res= new ArrayList<>();
-        preOrder(root,"");
+        // preOrder(root,"");
+        preOrder(root,new StringBuilder());
         return res;
     }
 
-    public void preOrder(TreeNode node, String cur){
+    // public void preOrder(TreeNode node, String cur){
+    public void preOrder(TreeNode node, StringBuilder cur){
         if(node == null)
             return;
-
-        if(cur.length()>0)
-            cur = cur + "->"+node.val;
-        else
-            cur = String.valueOf(node.val);
+        int len = cur.length();
+        cur.append(node.val);
+        // if(cur.length()>0)
+        //     cur = cur + "->"+node.val;
+        // else
+        //     cur = String.valueOf(node.val);
 
         if(node.left == null && node.right==null){
             res.add(cur.toString());
-            return;
+            // return;
+        } else {
+            cur.append("->");
         }
         
         preOrder(node.left,cur);
         preOrder(node.right,cur);
         // res.add(cur.toString());
-        // cur.setLength(cur.length()-1);
+        cur.setLength(len);
     }
 }
